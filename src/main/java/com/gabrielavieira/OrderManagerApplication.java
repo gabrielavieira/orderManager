b0a2e8c5e5e6e27bd95b8cf887828046f547e1a8
Gabriela Vieira <gabrielaviheira@gmail.com>
2019-09-12 07:47:58 -0300

    creating instantiation operation

--- src/main/java/com/gabrielavieira/OrderManagerApplication.java
+++ src/main/java/com/gabrielavieira/OrderManagerApplication.java
@@ -1,13 +1,31 @@
 package com.gabrielavieira;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.gabrielavieira.domain.Category;
+import com.gabrielavieira.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class OrderManagerApplication {
+public class OrderManagerApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category informatica = new Category(null, "Informática");
+		Category escritorio = new Category(null, "Escritório");
+	
+		categoryRepository.saveAll(Arrays.asList(informatica, escritorio));
 	}
 
 }
